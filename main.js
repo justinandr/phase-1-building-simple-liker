216f7f2308d106f937d1691e99e39fe3f740e23f
Justin Rodriguez <justin.r@me.com>
2023-06-05 12:06:54 -0400

    complete lab

--- main.js
+++ main.js
@@ -2,7 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+function handleClick(e){
+  if (e.target.textContent === EMPTY_HEART){
+    mimicServerCall()
+    .then((res) => {
+      e.target.textContent = FULL_HEART
+      e.target.classList.add('activated-heart')
+    })
+    .catch((res) => {
+      document.querySelector('#modal').classList.remove('hidden')
+      document.querySelector('#modal-message').textContent = res
+      setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000)
+    })
+  }
+  else if (e.target.textContent === FULL_HEART){
+    mimicServerCall()
+    .then((res) => {
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove('activated-heart')
+    })
+    .catch((res) => {
+      document.querySelector('#modal').classList.remove('hidden')
+      document.querySelector('#modal-message').textContent = res
+      setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000)
+    })
+  }
+}
+
+const heart = document.getElementsByClassName('like-glyph')
+
+for (x of heart){
+   x.addEventListener('click', handleClick)
+}
 
 
 
